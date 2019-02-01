60e5256fab87504d5f1c6354ae5d1fe1cbd77b02
kepawni <a+kepawni@commodea.com>
2019-02-01 17:08:31 +0100

    Add Type class and spec for converting GraphQL data types

---
+++ spec/serge/CodeGenerator/TypeSpec.php
@@ -0,0 +1,226 @@
+<?php declare(strict_types=1);
+namespace spec\serge\Kepawni\Serge\CodeGenerator;
+
+use GraphQL\Type\Definition\IDType;
+use GraphQL\Type\Definition\InputObjectType;
+use GraphQL\Type\Definition\StringType;
+use Kepawni\Serge\CodeGenerator\Type;
+use PhpSpec\ObjectBehavior;
+use Ramsey\Uuid\Uuid;
+
+class TypeSpec extends ObjectBehavior
+{
+    function it_can_be_converted_from_a_GraphQL_ID_type(IDType $graphQlType)
+    {
+        $this->beConstructedFromGraphQlType($graphQlType, Uuid::class, '\\My\\Project\\');
+        $this->shortName->shouldBe('Uuid');
+        $this->namespace->shouldBe('\\Ramsey\\Uuid');
+        $this->isNullable->shouldBe(true);
+        $this->isCollection->shouldBe(false);
+        $this->isScalar->shouldBe(false);
+    }
+
+    function it_can_be_converted_from_a_GraphQL_input_object_type(InputObjectType $graphQlType)
+    {
+        $graphQlType->beConstructedWith([['name' => 'Money']]);
+        $this->beConstructedFromGraphQlType($graphQlType, Uuid::class, '\\My\\Project\\');
+        $this->shortName->shouldBe('Money');
+        $this->namespace->shouldBe('\\My\\Project\\');
+        $this->isNullable->shouldBe(true);
+        $this->isCollection->shouldBe(false);
+        $this->isScalar->shouldBe(false);
+    }
+
+    function it_can_be_converted_from_a_GraphQL_scalar_type(StringType $graphQlType)
+    {
+        $this->beConstructedFromGraphQlType($graphQlType, Uuid::class, '\\My\\Project\\');
+        $this->shortName->shouldBe('string');
+        $this->namespace->shouldBe(null);
+        $this->isNullable->shouldBe(true);
+        $this->isCollection->shouldBe(false);
+        $this->isScalar->shouldBe(true);
+    }
+
+    function it_exposes_the_namespace()
+    {
+        $this->beConstructedWith('ShortName', 'namespace');
+        $this->namespace->shouldBe('namespace');
+    }
+
+    function it_exposes_the_short_name()
+    {
+        $this->beConstructedWith('ShortName');
+        $this->shortName->shouldBe('ShortName');
+    }
+
+    function it_exposes_whether_it_is_a_collection()
+    {
+        $this->isCollection->shouldBeBool();
+    }
+
+    function it_exposes_whether_it_is_nullable()
+    {
+        $this->isNullable->shouldBeBool();
+    }
+
+    function it_exposes_whether_it_is_scalar()
+    {
+        $this->isScalar->shouldBeBool();
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Type::class);
+    }
+
+    function it_serializes_to_different_type_specifiers_when_mixed()
+    {
+        $this->beConstructedWith(Type::MIXED);
+        $this->toDocParam()->shouldBe('mixed|null');
+        $this->toDocReturn()->shouldBe('mixed|null');
+        $this->toParam()->shouldBe('');
+        $this->toReturn()->shouldBe('');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_mixed_collection()
+    {
+        $this->beConstructedWith(Type::MIXED, null, true, true);
+        $this->toDocParam()->shouldBe('mixed[]|iterable|null');
+        $this->toDocReturn()->shouldBe('mixed[]|iterable|null');
+        $this->toParam()->shouldBe('?iterable ');
+        $this->toReturn()->shouldBe(': ?iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_mixed()
+    {
+        $this->beConstructedWith(Type::MIXED, null, false);
+        $this->toDocParam()->shouldBe('mixed');
+        $this->toDocReturn()->shouldBe('mixed');
+        $this->toParam()->shouldBe('');
+        $this->toReturn()->shouldBe('');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_mixed_collection()
+    {
+        $this->beConstructedWith(Type::MIXED, null, false, true);
+        $this->toDocParam()->shouldBe('mixed[]|iterable');
+        $this->toDocReturn()->shouldBe('mixed[]|iterable');
+        $this->toParam()->shouldBe('iterable ');
+        $this->toReturn()->shouldBe(': iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_static()
+    {
+        $this->beConstructedWith(Type::STATIC);
+        $this->toDocParam()->shouldBe('static|null');
+        $this->toDocReturn()->shouldBe('static|null');
+        $this->toParam()->shouldBe('?static ');
+        $this->toReturn()->shouldBe(': ?self');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_static_collection()
+    {
+        $this->beConstructedWith(Type::STATIC, null, true, true);
+        $this->toDocParam()->shouldBe('static[]|iterable|null');
+        $this->toDocReturn()->shouldBe('static[]|iterable|null');
+        $this->toParam()->shouldBe('?iterable ');
+        $this->toReturn()->shouldBe(': ?iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_static()
+    {
+        $this->beConstructedWith(Type::STATIC, null, false);
+        $this->toDocParam()->shouldBe('static');
+        $this->toDocReturn()->shouldBe('static');
+        $this->toParam()->shouldBe('static ');
+        $this->toReturn()->shouldBe(': self');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_static_collection()
+    {
+        $this->beConstructedWith(Type::STATIC, null, false, true);
+        $this->toDocParam()->shouldBe('static[]|iterable');
+        $this->toDocReturn()->shouldBe('static[]|iterable');
+        $this->toParam()->shouldBe('iterable ');
+        $this->toReturn()->shouldBe(': iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_scalar()
+    {
+        $this->beConstructedWith(Type::FLOAT);
+        $this->toDocParam()->shouldBe('float|null');
+        $this->toDocReturn()->shouldBe('float|null');
+        $this->toParam()->shouldBe('?float ');
+        $this->toReturn()->shouldBe(': ?float');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_scalar_collection()
+    {
+        $this->beConstructedWith(Type::FLOAT, null, true, true);
+        $this->toDocParam()->shouldBe('float[]|iterable|null');
+        $this->toDocReturn()->shouldBe('float[]|iterable|null');
+        $this->toParam()->shouldBe('?iterable ');
+        $this->toReturn()->shouldBe(': ?iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_scalar()
+    {
+        $this->beConstructedWith(Type::FLOAT, null, false);
+        $this->toDocParam()->shouldBe('float');
+        $this->toDocReturn()->shouldBe('float');
+        $this->toParam()->shouldBe('float ');
+        $this->toReturn()->shouldBe(': float');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_scalar_collection()
+    {
+        $this->beConstructedWith(Type::FLOAT, null, false, true);
+        $this->toDocParam()->shouldBe('float[]|iterable');
+        $this->toDocReturn()->shouldBe('float[]|iterable');
+        $this->toParam()->shouldBe('iterable ');
+        $this->toReturn()->shouldBe(': iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_object()
+    {
+        $this->beConstructedWith('Uuid', '\\Ramsey\\Uuid');
+        $this->toDocParam()->shouldBe('Uuid|null');
+        $this->toDocReturn()->shouldBe('Uuid|null');
+        $this->toParam()->shouldBe('?Uuid ');
+        $this->toReturn()->shouldBe(': ?Uuid');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_object_collection()
+    {
+        $this->beConstructedWith('Uuid', '\\Ramsey\\Uuid', true, true);
+        $this->toDocParam()->shouldBe('Uuid[]|iterable|null');
+        $this->toDocReturn()->shouldBe('Uuid[]|iterable|null');
+        $this->toParam()->shouldBe('?iterable ');
+        $this->toReturn()->shouldBe(': ?iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_object()
+    {
+        $this->beConstructedWith('Uuid', '\\Ramsey\\Uuid', false);
+        $this->toDocParam()->shouldBe('Uuid');
+        $this->toDocReturn()->shouldBe('Uuid');
+        $this->toParam()->shouldBe('Uuid ');
+        $this->toReturn()->shouldBe(': Uuid');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_required_object_collection()
+    {
+        $this->beConstructedWith('Uuid', '\\Ramsey\\Uuid', false, true);
+        $this->toDocParam()->shouldBe('Uuid[]|iterable');
+        $this->toDocReturn()->shouldBe('Uuid[]|iterable');
+        $this->toParam()->shouldBe('iterable ');
+        $this->toReturn()->shouldBe(': iterable');
+    }
+
+    function it_serializes_to_different_type_specifiers_when_void()
+    {
+        $this->beConstructedWith(Type::VOID);
+        $this->toDocReturn()->shouldBe('void');
+        $this->toReturn()->shouldBe(': void');
+    }
+}
---
+++ src/CodeGenerator/Type.php
@@ -0,0 +1,125 @@
+<?php declare(strict_types=1);
+namespace Kepawni\Serge\CodeGenerator;
+
+use GraphQL\Type\Definition\InputObjectType;
+use GraphQL\Type\Definition\ListOfType;
+use GraphQL\Type\Definition\NonNull;
+use GraphQL\Type\Definition\ScalarType;
+use GraphQL\Type\Definition\Type as GraphQlType;
+use Kepawni\Twilted\Basic\ImmutableValue;
+use UnexpectedValueException;
+
+/**
+ * @property-read string|null $shortName
+ * @property-read string|null $namespace
+ * @property-read bool|null $isNullable
+ * @property-read bool|null $isCollection
+ * @property-read bool|null $isScalar
+ * @method self withShortName(?string $v)
+ * @method self withNamespace(?string $v)
+ * @method self withIsNullable(?bool $v)
+ * @method self withIsCollection(?bool $v)
+ * @method self withIsScalar(?bool $v)
+ */
+class Type extends ImmutableValue
+{
+    public const BOOL = 'bool';
+    public const FLOAT = 'float';
+    public const INT = 'int';
+    public const MIXED = null;
+    public const SELF = 'self';
+    public const STATIC = 'static';
+    public const STRING = 'string';
+    public const VOID = 'void';
+
+    public function __construct(
+        ?string $shortName = null,
+        ?string $namespace = null,
+        bool $isNullable = true,
+        bool $isCollection = false,
+        bool $isScalar = false
+    ) {
+        $this->init('shortName', $shortName === 'mixed' ? null : $shortName);
+        $this->init('namespace', $namespace);
+        $this->init('isNullable', $isNullable && $shortName !== self::VOID);
+        $this->init('isCollection', $isCollection && $shortName !== self::VOID);
+        $this->init('isScalar', $isScalar);
+    }
+
+    public static function fromGraphQlType(GraphQlType $type, string $idClass, string $defaultNamespace)
+    {
+        return self::traverseGraphQlType(
+            $type,
+            new self(self::short($idClass), self::namespace($idClass)),
+            $defaultNamespace
+        );
+    }
+
+    public static function namespace(string $className): string
+    {
+        return implode('\\', array_slice(explode('\\', '\\' . trim($className, '\\')), 0, -1));
+    }
+
+    public static function short(string $className): string
+    {
+        return strval(array_reverse(explode('\\', $className))[0]);
+    }
+
+    private static function traverseGraphQlType(GraphQlType $type, self $result, string $defaultNamespace): self
+    {
+        if ($type instanceof NonNull) {
+            return $result->withIsNullable(false);
+        } elseif ($type instanceof ListOfType) {
+            return $result->withIsCollection(true);
+        } elseif ($type instanceof ScalarType) {
+            switch ($type->name) {
+                case GraphQlType::BOOLEAN:
+                    return $result->withIsScalar(true)->withShortName(self::BOOL)->withNamespace(null);
+                case GraphQlType::FLOAT:
+                    return $result->withIsScalar(true)->withShortName(self::FLOAT)->withNamespace(null);
+                case GraphQlType::ID:
+                    return $result;
+                case GraphQlType::INT:
+                    return $result->withIsScalar(true)->withShortName(self::INT)->withNamespace(null);
+                case GraphQlType::STRING:
+                    return $result->withIsScalar(true)->withShortName(self::STRING)->withNamespace(null);
+            }
+        } elseif ($type instanceof InputObjectType) {
+            return $result->withShortName($type->name)->withNamespace($defaultNamespace);
+        }
+        throw new UnexpectedValueException(
+            sprintf('Unsupported GraphQL type “%s”', $type->name ?: get_class($type))
+        );
+    }
+
+    public function toDocParam()
+    {
+        return ($this->shortName ?: 'mixed')
+            . ($this->isCollection ? '[]|iterable' : '')
+            . ($this->isNullable ? '|null' : '');
+    }
+
+    public function toDocReturn()
+    {
+        return ($this->shortName ?: 'mixed')
+            . ($this->isCollection ? '[]|iterable' : '')
+            . ($this->isNullable ? '|null' : '');
+    }
+
+    public function toParam()
+    {
+        return $this->shortName || $this->isCollection
+            ? ($this->isNullable ? '?' : '') . ($this->isCollection ? 'iterable' : $this->shortName) . ' '
+            : '';
+    }
+
+    public function toReturn()
+    {
+        return $this->shortName || $this->isCollection
+            ? (': '
+                . ($this->isNullable ? '?' : '')
+                . ($this->isCollection ? 'iterable' : ($this->shortName === 'static' ? 'self' : $this->shortName))
+            )
+            : '';
+    }
+}
