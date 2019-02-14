7d5a8fb378036ec9a3d74871679f724b73ca9c38
kepawni <a+kepawni@commodea.com>
2019-02-14 08:29:04 +0100

    Add Method and Classifier based on code block implementations

---
+++ src/CodeGenerator/Classifier.php
@@ -0,0 +1,221 @@
+<?php declare(strict_types=1);
+namespace Kepawni\Serge\CodeGenerator;
+
+class Classifier
+{
+    private $abstract;
+    private $constants;
+    private $final;
+    private $interface;
+    private $interfaces;
+    private $methods;
+    private $name;
+    private $namespace;
+    private $parent;
+    private $properties;
+    private $usedClasses;
+
+    public function __construct(string $name, string $namespace = '')
+    {
+        $this->abstract = false;
+        $this->constants = [];
+        $this->final = false;
+        $this->interface = false;
+        $this->interfaces = [];
+        $this->methods = [];
+        $this->name = $name;
+        $this->namespace = $namespace;
+        $this->parent = null;
+        $this->properties = [];
+        $this->usedClasses = [];
+    }
+
+    public function __toString(): string
+    {
+        sort($this->usedClasses);
+        sort($this->interfaces);
+        ksort($this->constants);
+        usort($this->properties, [$this, 'compareProperties']);
+        usort($this->methods, [$this, 'compareMethods']);
+        $classBlock = new IndentedMultilineBlock('{', '}', "\n");
+        if ($this->constants) {
+            $classBlock->addContentString(
+                implode(
+                    "\n",
+                    array_map([$this, 'formatConstant'], array_keys($this->constants), array_values($this->constants))
+                )
+            );
+        }
+        if ($this->properties) {
+            $classBlock->addContentString(implode(";\n", $this->properties) . ';');
+        }
+        $classBlock->addContentString(
+            implode(
+                "\n\n",
+                $this->interface
+                    ? array_map([$this, 'methodSignature'], $this->methods)
+                    : $this->methods
+            )
+        );
+        return sprintf(
+            "<?php declare(strict_types=1);\n%s%s\n%s%s%s%s%s%s\n%s\n",
+            $this->namespace ? 'namespace ' . trim($this->namespace, '\\') . ";\n" : '',
+            $this->usedClasses ? "\nuse " . implode(";\nuse ", array_unique($this->usedClasses)) . ";\n" : '',
+            $this->abstract ? 'abstract ' : '',
+            $this->final ? 'final ' : '',
+            $this->interface ? 'interface ' : 'class ',
+            $this->name,
+            $this->parent ? ' extends ' . $this->parent : '',
+            $this->interfaces
+                ? ($this->isInterface() ? ' extends ' : ' implements ') . implode(', ', array_unique($this->interfaces))
+                : '',
+            $classBlock
+        );
+    }
+
+    public function addConstant(string $name, $value): self
+    {
+        $this->constants[$name] = json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        return $this;
+    }
+
+    public function addMethod(Method $method): self
+    {
+        $this->methods[] = $method;
+        $this->usedClasses = array_merge($this->usedClasses, $method->getUsedClasses());
+        return $this;
+    }
+
+    public function addProperty(Property $property): self
+    {
+        $this->properties[] = $property;
+        if ($property->getType()->getNamespace() !== null) {
+            $this->usedClasses[] = trim($property->getType()->getFullName(), '\\');
+        }
+        return $this;
+    }
+
+    public function extend(Classifier $classifier): self
+    {
+        if ($this->isInterface()) {
+            $this->interfaces[] = $classifier->getName();
+        } else {
+            $this->parent = $classifier->getName();
+        }
+        $this->usedClasses[] = trim($classifier->getFullName(), '\\');
+        return $this;
+    }
+
+    public function getConstants(): array
+    {
+        return $this->constants;
+    }
+
+    public function getFullName()
+    {
+        return '\\' . trim($this->namespace, '\\') . '\\' . $this->name;
+    }
+
+    public function getMethods(): array
+    {
+        return $this->methods;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getNamespace(): string
+    {
+        return $this->namespace;
+    }
+
+    public function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    public function implement(Classifier $interface): self
+    {
+        $this->interfaces[] = $interface->getName();
+        $this->usedClasses[] = trim($interface->getFullName(), '\\');
+        return $this;
+    }
+
+    public function isAbstract(): bool
+    {
+        return $this->abstract;
+    }
+
+    public function isFinal(): bool
+    {
+        return $this->final;
+    }
+
+    public function isInterface(): bool
+    {
+        return $this->interface;
+    }
+
+    public function makeAbstract(bool $value = true): self
+    {
+        $this->abstract = $value;
+        if ($value) {
+            $this->final = false;
+            $this->interface = false;
+        }
+        return $this;
+    }
+
+    public function makeFinal(bool $value = true): self
+    {
+        $this->final = $value;
+        if ($value) {
+            $this->abstract = false;
+            $this->interface = false;
+        }
+        return $this;
+    }
+
+    public function makeInterface(bool $value = true): self
+    {
+        $this->interface = $value;
+        if ($value) {
+            $this->abstract = false;
+            $this->final = false;
+        }
+        return $this;
+    }
+
+    private function compareMethods(Method $a, Method $b): int
+    {
+        return intval($b->isStatic()) - intval($a->isStatic())
+            ?: (
+                intval($a->isProtected()) + 2 * intval($a->isPublic())
+                - intval($b->isProtected()) - 2 * intval($a->isPublic())
+            )
+                ?: strcmp($a->getName(), $b->getName());
+    }
+
+    private function compareProperties(Property $a, Property $b): int
+    {
+        return intval($b->isStatic()) - intval($a->isStatic())
+            ?: intval($b->getName() === '__construct') - intval($a->getName() === '__construct')
+                ?: (
+                    intval($a->isProtected()) + 2 * intval($a->isPublic())
+                    - intval($b->isProtected()) - 2 * intval($a->isPublic())
+                )
+                    ?: strcmp($a->getName(), $b->getName());
+    }
+
+    private function formatConstant(string $name, string $value): string
+    {
+        return sprintf('const %s = %s;', $name, $value);
+    }
+
+    private function methodSignature(Method $method): string
+    {
+        return $method->generateSignature() . ';';
+    }
+}
---
+++ src/CodeGenerator/Method.php
@@ -0,0 +1,203 @@
+<?php declare(strict_types=1);
+namespace Kepawni\Serge\CodeGenerator;
+
+use ReflectionMethod;
+use ReflectionParameter;
+
+class Method extends IndentedMultilineBlock
+{
+    private $abstract;
+    private $final;
+    private $name;
+    private $parameters;
+    private $returnType;
+    private $static;
+    private $visibility;
+
+    public function __construct(string $name)
+    {
+        parent::__construct();
+        $this->abstract = false;
+        $this->final = false;
+        $this->name = $name;
+        $this->parameters = [];
+        $this->returnType = new Type(Type::MIXED);
+        $this->static = false;
+        $this->visibility = 'public';
+    }
+
+    public function __toString(): string
+    {
+        return $this->generateSignature() . ($this->isAbstract()
+                ? ';'
+                : PHP_EOL
+                . $this->getPrefix()
+                . $this->contentToString()
+                . $this->getSuffix());
+    }
+
+    public function appendParameter(Parameter $parameter): self
+    {
+        $this->parameters[] = $parameter;
+        if ($parameter->getType()->getNamespace() !== null) {
+            $this->useClass($parameter->getType()->getFullName());
+        }
+        return $this;
+    }
+
+    public function equalsSignatureOf(ReflectionMethod $existingMethod)
+    {
+        return $this->generateSignature() === $this->generateSignatureFromReflection($existingMethod);
+    }
+
+    public function generateSignature(): string
+    {
+        return sprintf(
+            "%s%s%s %sfunction %s(%s)%s",
+            $this->abstract ? 'abstract ' : '',
+            $this->final ? 'final ' : '',
+            $this->visibility,
+            $this->static ? 'static ' : '',
+            $this->name,
+            implode(', ', $this->parameters),
+            $this->returnType->toReturn()
+        );
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getReturnType(): Type
+    {
+        return $this->returnType;
+    }
+
+    public function isAbstract(): bool
+    {
+        return $this->abstract;
+    }
+
+    public function isFinal(): bool
+    {
+        return $this->final;
+    }
+
+    public function isPrivate(): bool
+    {
+        return $this->visibility === 'private';
+    }
+
+    public function isProtected(): bool
+    {
+        return $this->visibility === 'protected';
+    }
+
+    public function isPublic(): bool
+    {
+        return $this->visibility === 'public';
+    }
+
+    public function isStatic(): bool
+    {
+        return $this->static;
+    }
+
+    public function makeAbstract(bool $value = true): self
+    {
+        $this->abstract = $value;
+        if ($value) {
+            $this->final = false;
+        }
+        return $this;
+    }
+
+    public function makeFinal(bool $value = true): self
+    {
+        $this->final = $value;
+        if ($value) {
+            $this->abstract = false;
+        }
+        return $this;
+    }
+
+    public function makePrivate(): self
+    {
+        $this->visibility = 'private';
+        return $this;
+    }
+
+    public function makeProtected(): self
+    {
+        $this->visibility = 'protected';
+        return $this;
+    }
+
+    public function makePublic(): self
+    {
+        $this->visibility = 'public';
+        return $this;
+    }
+
+    public function makeReturn(Type $returnType): self
+    {
+        $this->returnType = $returnType;
+        if ($returnType->getNamespace() !== null) {
+            $this->useClass($returnType->getFullName());
+        }
+        return $this;
+    }
+
+    public function makeStatic(bool $value = true): self
+    {
+        $this->static = $value;
+        return $this;
+    }
+
+    private function generateParameterFromReflection(ReflectionParameter $parameter): string
+    {
+        return trim(
+            sprintf(
+                '%s%s $%s%s',
+                $parameter->hasType() && $parameter->getType()->allowsNull() ? '?' : '',
+                $parameter->hasType() ? array_reverse(explode('\\', $parameter->getType()->getName()))[0] : '',
+                $parameter->getName(),
+                $parameter->isDefaultValueAvailable()
+                    ? ' = ' . ($parameter->isDefaultValueConstant()
+                        ? $parameter->getDefaultValueConstantName()
+                        : json_encode($parameter->getDefaultValue())
+                    )
+                    : ''
+            )
+        );
+    }
+
+    private function generateSignatureFromReflection(ReflectionMethod $method): string
+    {
+        return sprintf(
+            '%s(%s)%s',
+            implode(
+                ' ',
+                [
+                    $method->isAbstract() ? 'abstract' : '',
+                    $method->isFinal() ? 'final' : '',
+                    $method->isPrivate() ? 'private' : '',
+                    $method->isProtected() ? 'protected' : '',
+                    $method->isPublic() ? 'public' : '',
+                    $method->isStatic() ? 'static' : '',
+                    'function',
+                    $method->getName(),
+                ]
+            ),
+            implode(', ', array_map([$this, 'generateParameterFromReflection'], $method->getParameters())),
+            $method->hasReturnType()
+                ? sprintf(
+                ': %s%s',
+                $method->getReturnType()->allowsNull() ? '?' : '',
+                array_reverse(explode('\\', $method->getReturnType()->getName()))[0]
+            )
+                : ''
+        );
+    }
+}
